cbb9c037578f4a1c1e00d86704d491b9efad2322
harshal jain <harshal.jain@koireader.com>
2024-06-28 13:22:41 +0530

    added DL function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  function openApp() {
+    const appURL = "deeplinkinghj://product/1"; // Custom scheme URL for your app
+    const fallbackURL = window.location.href; // Current web URL
+
+    // Create an iframe to attempt to open the app
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.src = appURL;
+    document.body.appendChild(iframe);
+
+    // Remove iframe after 2 seconds and redirect to the fallback URL
+    setTimeout(() => {
+      document.body.removeChild(iframe);
+      window.location.href = fallbackURL;
+    }, 2000);
+  }
+
+  useEffect(() => {
+    openApp();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +39,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <button onClick={() => openApp()}>OPEN APP</button>
       </header>
     </div>
   );
